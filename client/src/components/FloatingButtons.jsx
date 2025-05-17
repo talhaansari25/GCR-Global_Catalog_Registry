e51a13fb1c01116e56c2e71c836d3fa9c0e41463
Talha <talhaansari2026@gmail.com>
2025-05-17 14:32:44 +0530

    float

--- client/src/components/FloatingButtons.jsx
+++ client/src/components/FloatingButtons.jsx
@@ -1,28 +1,42 @@
 import React from 'react'
 
-export default function FloatingButtons({isAbsolute,setSellerState,activeSingle, setActiveSingle}) {
+export default function FloatingButtons({ isAbsolute, setSellerState, activeSingle, setActiveSingle }) {
   return (
     <div className={`floatButtons ${isAbsolute ? "absolute" : ""}`} >
-        <div className='singleUpload' 
-          onClick={()=>{
-            setSellerState("Home");
-            setActiveSingle(true);
-          }}
-        ><i className='fas fa-upload'></i></div>
-        <div className='bulkUpload' 
-          onClick={()=>{
-            setSellerState("Home");
-            setActiveSingle(false);
-          }}
-        ><i className='fas fa-file-csv'></i></div>
-        <div className='updateProd' 
-        onClick={()=>{
+      <div
+        className='singleUpload'
+        onClick={() => {
+          setSellerState("Home");
+          setActiveSingle(true);
+        }}
+      >
+        <i className='fas fa-upload'></i>
+      </div>
+      <div
+        className='bulkUpload'
+        onClick={() => {
+          setSellerState("Home");
+          setActiveSingle(false);
+        }}
+      >
+        <i className='fas fa-file-csv'></i>
+      </div>
+      <div
+        className='updateProd'
+        onClick={() => {
           setSellerState("Products");
-        }}><i className='fas fa-pen'></i></div>
-        <div className='insights' 
-        onClick={()=>{
+        }}
+      >
+        <i className='fas fa-pen'></i>
+      </div>
+      <div
+        className='insights'
+        onClick={() => {
           setSellerState("Insights");
-        }}><i className='fas fa-chart-simple'></i></div>
+        }}
+      >
+        <i className='fas fa-chart-simple'></i>
+      </div>
     </div>
   )
 }
