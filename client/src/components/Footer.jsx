aa7f871451301154f3a9be40ea4f0d2875a5ff89
Talha <talhaansari2026@gmail.com>
2025-05-17 14:33:06 +0530

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='footer'> &copy; 2025 All Rights Reserved</div>
+    <div className='footer'>
+      &copy; 2025 All Rights Reserved
+    </div>
   )
 }
