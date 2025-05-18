6932df0a9076b4eb008d295b11447661e9d6d41d
Talha <talhaansari2026@gmail.com>
2025-05-18 20:48:10 +0530

    seller

--- client/src/components/SellerMain.jsx
+++ client/src/components/SellerMain.jsx
@@ -9,25 +9,27 @@ import Support from "./seller/Support";
 import Settings from "./seller/Settings";
 // import Products from "./Products";
 
-export default function SellerMain({sid,sellerState,bottomMarkerRef, activeSingle, setActiveSingle,searchQuery,searchRes}) {
-  
+export default function SellerMain({ sid, sellerState, bottomMarkerRef, activeSingle, setActiveSingle, searchQuery, searchRes }) {
+
   return (
     <>
       <div className='sellerMain'>
-        
-        {sellerState=="Home" && <SellerForm activeSingle={activeSingle} setActiveSingle={setActiveSingle} sid={sid}/>}
-        {sellerState=="Profile" && <Profile sid={sid}/>}
-        {sellerState=="Insights" && <Insights sid={sid}/>}
-        {sellerState=="Products" && <Sproducts searchQuery={searchQuery} searchRes={searchRes} sid={sid}/>}
+
+        {sellerState == "Home" && <SellerForm activeSingle={activeSingle} setActiveSingle={setActiveSingle} sid={sid} />}
+        {sellerState == "Profile" && <Profile sid={sid} />}
+        {sellerState == "Insights" && <Insights sid={sid} />}
+        {sellerState == "Products" && <Sproducts searchQuery={searchQuery} searchRes={searchRes} sid={sid} />}
         {/* {sellerState=="Products" && <Products searchQuery={""} searchRes={[]}/>} */}
-        {sellerState=="Drafts" && <Drafts/>}
-        {sellerState=="Notification" && <Notification/>}
-        {sellerState=="Support" && <Support/>}
-        {sellerState=="Settings" && <Settings/>}
+        {sellerState == "Drafts" && <Drafts />}
+        {sellerState == "Notification" && <Notification />}
+        {sellerState == "Support" && <Support />}
+        {sellerState == "Settings" && <Settings />}
 
-        <div ref={bottomMarkerRef} style={{ margin: "5px", height: "1px", background: "transparent" }}></div>
+        <div
+          ref={bottomMarkerRef}
+          style={{ margin: "5px", height: "1px", background: "transparent" }}
+        ></div>
 
-        
       </div>
     </>
   )
